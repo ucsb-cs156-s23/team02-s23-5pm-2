aaac779383c37daee2f1d173173c1fb7305ddbec
jtmnguyen <jessicanguyen@ucsb.edu>
2023-05-11 14:50:27 -0700

    jn-deleted function from repository

--- src/main/java/edu/ucsb/cs156/example/repositories/RestaurantRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/RestaurantRepository.java
@@ -7,5 +7,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestaurantRepository extends CrudRepository<Restaurant, Long> {
-    Iterable<Restaurant> findAllByTakeOut(boolean hasTakeOut);
 }
